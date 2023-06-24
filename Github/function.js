42fd45086931899833cca6565151407a78306764
er3nnz <er3naksu@gmail.com>
2023-06-24 14:06:42 +0300

    Js İle Basic Traffic Lamb Tasarımı

---
+++ Github/function.js
@@ -0,0 +1,23 @@
+document.getElementById("kırmızı").addEventListener("click",kırmızı);
+
+
+function kırmızı(){
+ 
+    document.getElementById("red").style.background="red";
+}
+
+
+document.getElementById("sarı").addEventListener("click",sarı);
+
+function sarı(){
+
+   document.getElementById("yellow").style.background="yellow";
+}
+
+
+document.getElementById("yesil").addEventListener("click",yesil);
+
+function yesil(){
+
+    document.getElementById("green").style.background="green";
+}
